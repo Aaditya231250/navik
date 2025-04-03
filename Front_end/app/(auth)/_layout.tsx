26511b947b76fd0423f0cc951486a36b67d62a34
japneet <b23cs1022@iitj.ac.in>
2025-04-03 18:16:12 +0530

    Authentication

--- Front_end/app/(auth)/_layout.tsx
+++ Front_end/app/(auth)/_layout.tsx
@@ -1,5 +1,3 @@
-// @ts-ignore
-
 import { Stack } from "expo-router";
 
 const Layout = () => {
--- Front_end/app/index.tsx
+++ Front_end/app/index.tsx
@@ -1,9 +1,30 @@
-import { Text } from "react-native"; // Importing components from React Native
-import { SafeAreaView } from "react-native-safe-area-context";
+// import { Text } from "react-native"; // Importing components from React Native
+// import { SafeAreaView } from "react-native-safe-area-context";
+// import { Redirect } from "expo-router";
+// import { useAuth } from "@clerk/clerk-expo";
+//
+// const Home = () => {
+//   // Functional component
+//   const { isSignedIn } = useAuth();
+//
+//   if (isSignedIn) {
+//     return <Redirect href={"/"} />;
+//   }
+//
+//   return <Redirect href={"/(auth)/welcome"} />;
+// };
+//
+// export default Home; // Exporting the component
+
+import { useAuth } from "@clerk/clerk-expo";
 import { Redirect } from "expo-router";
-const Home = () => {
-  // Functional component
-  return <Redirect href={"/(auth)/welcome"} />;
+
+const Page = () => {
+  const { isSignedIn } = useAuth();
+
+  if (isSignedIn) return <Redirect href="/(root)/(tabs)/home" />;
+
+  return <Redirect href="/(auth)/welcome" />;
 };
 
-export default Home; // Exporting the component
+export default Page;
